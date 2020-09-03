7fc2536c77257c67eaa6787c3cfb1556825d57d3
fjavadi <44327920+fjavadi@users.noreply.github.com>
2020-09-03 05:08:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function start(func) {
+  if(document.readyState != 'loading') {
+    func();
+  } else {
+    document.addEventListener('DOMContentLoaded', func);
+  }
+}
+
+var rating = 0;
+
+start(function(){
+
+  stars = document.getElementsByClassName("star");
+  myCanvas = document.getElementById("myCanvas").getContext("2d");
+  myCanvas.font = "20px Arial"
+  myCanvas.fillStyle = "#26517a";
+
+  for (var i = 0; i < 5; i++){
+
+    stars[i].addEventListener("click", function(event){
+      console.log("click");
+      rating = parseInt(event.target.getAttribute('id')) + 1;
+
+      for (j = 0; j < 5; j++){
+        stars[j].classList.remove("hover");
+        if(j < rating){
+          stars[j].classList.add("selected");
+        }
+      }
+      event.preventDefault();
+
+      // print the rating value
+      myCanvas.clearRect(0, 0, 200, 30);
+      myCanvas.fillText("Your rating: " + rating, 0, 20);
+
+    });
+
+    stars[i].addEventListener("mouseover", function(event){
+      for (var j = 0; j < 5; j++){
+        stars[j].classList.remove("selected");
+
+        if (j <= parseInt(event.target.getAttribute('id'))){
+          stars[j].classList.add("hover");
+        }
+      }
+    });
+
+    stars[i].addEventListener("mouseout", function(){
+      for (var j = 0; j < 5; j++){
+        stars[j].classList.remove("hover");
+      }
+      if (rating != 0){
+        for (var j = 0; j < rating; j++){
+          stars[j].classList.add("selected");
+        }
+      }
+    });
+  }
+});
